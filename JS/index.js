7225e31d7c93caef39cd9482df35646915ad37a9
JeremyK <99058701@mydacinci.nl>
2021-02-11 21:56:33 +0100

    Basic work on pagination
    added JS functions to make the system run on 1 page

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,28 @@
 //remember querySelectorAll for multi-buttons
 
+//region -Variables-
+
+//region -Elements-
+var start_Button = document.getElementById("button_start");
+var top_Container = document.getElementById("top_container");
+var nav_Container = document.getElementById("top_nav");
+var nav_Back_Button = document.getElementById("top_nav_back_button");
+var main_Container = document.getElementById("main_container");
+var statement_Container = document.getElementById("statement_container");
+//end-region
+
+//region -Standard-
+var currentStatement = 0;
+//end-region
+
+//end-region
+
+//region -events-
+start_Button.addEventListener("click", updatePageForStatements);
+nav_Back_Button.addEventListener("click", updatePageForPreviousStatement)
+//end-region
+
+//region -functions-
 function createPartiesParticipating(){
     var participatingLength = partyArrayParticipating.length;
     var container = document.querySelectorAll(".start__parties-list")[0] //get first list of parties
@@ -52,5 +75,32 @@ function createPartiesNotParticipating(){
     }
 }
 
+function updatePageForStatements(){
+    //Do styling options
+    top_Container.classList.add("app--white");
+    main_Container.classList.add("hidden");
+    start_Button.classList.add("button--disabled");
+
+    nav_Container.classList.remove("app__nav--hidden");
+    nav_Back_Button.classList.remove("button--disabled");
+    statement_Container.classList.remove("hidden");
+    window.history.pushState('', subjects[0].title, '?1')//update the url parameters without reloading page as reloading will default to home page
+}
+
+function updatePageForPreviousStatement(){
+    currentStatement = parseInt(window.location.search.replace('?', ''));
+    if(currentStatement <= 1){
+        window.location.href = "index.html";
+        return;
+    }
+}
+
+function updatePageForNextStatement(){
+
+}
+//end-region
+
+//region -execute-
 createPartiesParticipating();
 createPartiesNotParticipating();
+//end-region
