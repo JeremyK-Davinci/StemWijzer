e8cd678b00b5d1a461ec0fd98e38a92220460ae5
JeremyK <99058701@mydacinci.nl>
2021-02-12 12:08:10 +0100

    Improved backend
    added basic functionality

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-//remember querySelectorAll for multi-buttons
+//remember querySelectorAll for multi-Elements
 
 //region -Variables-
 
@@ -9,10 +9,39 @@ var nav_Container = document.getElementById("top_nav");
 var nav_Back_Button = document.getElementById("top_nav_back_button");
 var main_Container = document.getElementById("main_container");
 var statement_Container = document.getElementById("statement_container");
+var statement_Current_Count = document.getElementById("counter");
+
+var agree_Button = document.querySelector(".button--agree");
+var disagree_Button = document.querySelector(".button--disagree");
+var no_Choice_Button = document.querySelector(".button--neither");
+var skip_Button = document.querySelector(".statement__skip");
+var statement_Title = document.querySelector(".statement__theme");
+var statement_Text = document.querySelector(".statement-title");
+var statement_Tab_Parties = document.querySelector(".statement__tab--parties");
+var statement_Tab_Info = document.querySelector(".statement__tab--more-info");
+var statement_Tab_Button_Parties = document.querySelector(".statement__tab-button--parties");
+var statement_Tab_Button_Info = document.querySelector(".statement__tab-button--more-info");
+var statement_Tab_Button_Close = document.querySelectorAll(".statement__tab-close");
+
+var statement_Parties_Columns = document.querySelectorAll(".statement__parties-column");
+
+var statement_Info_Text = document.querySelector(".statement__tab-text");
 //end-region
 
 //region -Standard-
 var currentStatement = 0;
+var showingPartyInfo = false;
+var showingInfo = false;
+//end-region
+
+//region -Arrays-
+var choices = [];
+//end-region
+
+//region -constants-
+const indexAgree = 0;
+const indexNeither = 1;
+const indexDisagree = 2;
 //end-region
 
 //end-region
@@ -20,6 +49,17 @@ var currentStatement = 0;
 //region -events-
 start_Button.addEventListener("click", updatePageForStatements);
 nav_Back_Button.addEventListener("click", updatePageForPreviousStatement)
+
+agree_Button.addEventListener("click", updatePageForNextStatement);
+disagree_Button.addEventListener("click", updatePageForNextStatement);
+no_Choice_Button.addEventListener("click", updatePageForNextStatement);
+skip_Button.addEventListener("click", updatePageForNextStatement);
+
+statement_Tab_Button_Parties.addEventListener("click", updateStatementPartyInfo);
+statement_Tab_Button_Info.addEventListener("click", updateStatementInfo);
+statement_Tab_Button_Close.forEach(button => {
+    button.addEventListener("click", closeCurrentTab);
+});
 //end-region
 
 //region -functions-
@@ -84,19 +124,121 @@ function updatePageForStatements(){
     nav_Container.classList.remove("app__nav--hidden");
     nav_Back_Button.classList.remove("button--disabled");
     statement_Container.classList.remove("hidden");
-    window.history.pushState('', subjects[0].title, '?1')//update the url parameters without reloading page as reloading will default to home page
+    currentStatement++;
+    updateStatementHTML(currentStatement);
 }
 
 function updatePageForPreviousStatement(){
-    currentStatement = parseInt(window.location.search.replace('?', ''));
-    if(currentStatement <= 1){
+    if(currentStatement <= 1){ //If this is the first statement go to the home page
         window.location.href = "index.html";
         return;
     }
+
+    choices.pop(); //Remove the last statement and choice from array
+    currentStatement--;
+    statement_Current_Count.innerHTML = currentStatement + '/';
+    updateStatementHTML(currentStatement);
 }
 
 function updatePageForNextStatement(){
+    if(currentStatement == 30){ //If this is the last statement go to th home page
+        window.location.href = "index.html";
+        return;
+    }
 
+    choices.push({id:currentStatement, choice:this.id}) //Add the currentStatement and it's choice to the choices
+    currentStatement++;
+    statement_Current_Count.innerHTML = currentStatement + '/';
+    updateStatementHTML(currentStatement - 1); //Show the statement (-1 because arrays start at 0)
+}
+
+function updateStatementHTML(id){
+    statement_Title.innerHTML = subjects[id].title;
+    statement_Text.innerHTML = subjects[id].statement;
+}
+
+function updateStatementPartyInfo(){
+    if(statement_Tab_Parties.classList.contains("hidden")){ //Not showing
+        statement_Tab_Info.classList.add("hidden");
+        statement_Tab_Parties.classList.remove("hidden");
+
+        var currentSubject = subjects[currentStatement - 1];
+
+        currentSubject.parties.forEach(party => {
+            var partyFromArray = partyArrayParticipating.find(item => item.name == party.name);
+            if(partyFromArray !== null && partyFromArray !== undefined){
+                var rowElement = document.createElement("div");
+                var rowButtonElement = document.createElement("button");
+                var rowButtonElementLogo = document.createElement("div");
+                var rowButtonElementText = document.createElement("span");
+                var rowInfoElement = document.createElement("div");
+
+                rowInfoElement.classList.add("parties-column__party-explanation", "hidden");
+                rowInfoElement.innerHTML = party.opinion;
+
+                rowButtonElement.setAttribute("tabindex", "0");
+                rowButtonElement.classList.add("parties-column__party-header", "parties-column__party-header--with-explanation");
+                rowButtonElement.addEventListener("click", showPartyStanceInfo);
+
+                rowButtonElementLogo.classList.add("parties-column__party-logo");
+                var logoOffset = partyFromArray.logo_offset;
+                rowButtonElementLogo.setAttribute("style", "background: url(https://tweedekamer2021.stemwijzer.nl/gfx/img/parties.png) 0px " + logoOffset + "% / 100% 3000% no-repeat;");
+
+                rowButtonElementText.classList.add("parties-column__party-name");
+                rowButtonElementText.innerHTML = party.name;
+
+                rowButtonElement.appendChild(rowButtonElementLogo);
+                rowButtonElement.appendChild(rowButtonElementText);
+
+                rowElement.appendChild(rowButtonElement);
+                rowElement.appendChild(rowInfoElement);
+
+                switch(party.position){
+                    case "pro":
+                        statement_Parties_Columns[indexAgree].appendChild(rowElement);
+                        break;
+                    case "contra":
+                        statement_Parties_Columns[indexDisagree].appendChild(rowElement);
+                        break;
+                    case "none":
+                        statement_Parties_Columns[indexNeither].appendChild(rowElement);
+                        break;
+                }
+            }
+        })
+    }
+    else{ //Showing
+        statement_Tab_Parties.classList.add("hidden");
+    }
+}
+
+function updateStatementInfo(){
+    if(statement_Tab_Info.classList.contains("hidden")){ //Not showing
+        statement_Tab_Parties.classList.add("hidden");
+        statement_Tab_Info.classList.remove("hidden");
+
+        statement_Info_Text.innerHTML = "No Info";
+    }
+    else{ //Showing
+        statement_Tab_Info.classList.add("hidden");
+    }
+}
+
+function showPartyStanceInfo(){
+    var infoElement = this.nextElementSibling;
+    console.log(infoElement);
+    if(infoElement.classList.contains("hidden")){
+        infoElement.classList.remove("hidden");
+        this.classList.add("parties-column__party-header--open");
+    }
+    else{
+        infoElement.classList.add("hidden");
+        this.classList.remove("parties-column__party-header--open");
+    }
+}
+
+function closeCurrentTab(){
+    this.parentElement.classList.add("hidden");
 }
 //end-region
 
